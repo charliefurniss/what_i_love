defeaf319193a7415c9aaf3a3a69e5680885e03f
Charlie Furniss <charliefurniss96@gmail.com>
2016-03-14 09:21:39 +0000

    word seletced from array and shown on screen

--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
 
 	console.log('the dom is ready');
 
+	var mainWord = ["Wine", "Cooking", "Music"];
+
+	function chooseWord(wordsArray) {
+		
+		var arrayPosition = Math.floor((Math.random() * wordsArray.length));
+
+		return wordsArray[arrayPosition];
+
+	}
+
+	function showWordonScreen(word) {
+		$('#mainWord').html(word);
+	}
+
+	var selectedMainWord = chooseWord(mainWord);
+
+	showWordonScreen(selectedMainWord);
+
 });
