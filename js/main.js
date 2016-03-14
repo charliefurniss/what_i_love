e31f7c8b315003745b02cf9af82bf63d28a4303a
Charlie Furniss <charliefurniss96@gmail.com>
2016-03-14 11:08:42 +0000

    words displayed in table

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
 $(document).ready(function() {
 
-	console.log('the dom is ready');
-
-	var mainWord = ["Wine", "Cooking", "Music"];
-
-	function chooseWord(wordsArray) {
-		
-		var arrayPosition = Math.floor((Math.random() * wordsArray.length));
-
-		return wordsArray[arrayPosition];
-
+	var subjectArray = [
+	{
+		subject: "wine",
+		word1: 		"tasting",
+		word2: 		"Burgundy",
+		word3: 		"Brunello",
+		word4: 		"Hermitage",
+		word5: 		"Pinot Noir",
+		word6: 		"Chardonnay",
+		word7: 		"Gulp",
+		word8: 		"Slurp",
+	}, 
+	{
+		subject: "music",
+		word1: 		"Miles Davis",
+		word2: 		"Nile Rogers",
+		word3: 		"Fender guitars",
+		word4: 		"Fender Rhodes",
+		word5: 		"Fender amps",
+		word6: 		"Soul Jazz Revue",
+		word7: 		"More Boogie",
+		word8: 		"Funk",
+	}]
+	
+	function chooseWordObject(subjectArray) {
+		var arrayPosition = Math.floor((Math.random() * subjectArray.length));
+		return subjectArray[arrayPosition];
 	}
 
-	function showWordonScreen(word) {
-		$('#mainWord').html(word);
+	function showWordsonScreen(subjectObject) {
+		$('#subject').html(subjectObject.subject);
+		$('#word1').html(subjectObject.word1);
+		$('#word2').html(subjectObject.word2);
+		$('#word3').html(subjectObject.word3);
+		$('#word4').html(subjectObject.word4);
+		$('#word5').html(subjectObject.word5);
+		$('#word6').html(subjectObject.word6);
+		$('#word7').html(subjectObject.word7);
+		$('#word8').html(subjectObject.word8);
 	}
 
-	var selectedMainWord = chooseWord(mainWord);
+	var selectedSubjectObject = chooseWordObject(subjectArray);
 
-	showWordonScreen(selectedMainWord);
+	showWordsonScreen(selectedSubjectObject);
 
 });
